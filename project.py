86188bf4981a98276485eb591185ef39f8f0b1bd
nicklelate <worawutkongwee@gmail.com>
2022-08-09 10:23:48 +0700

    make it better

--- project.py
+++ project.py
@@ -243,6 +243,97 @@ async def mycoin(customer_id: str):
 
 
 
+#NFTs
+@app.get("/hobobag/{customer_id}")
+async def getcoin(customer_id: str, price: int):
+    users = contract_instance.functions.show_user().call()
+    wallet_index = 0
+    found = 0
+    for i in users:
+        if i == customer_id:
+            wallet_index = found
+        else:
+            found += 1
+    address = hardwallet[wallet_index][0]
+    privatekey = hardwallet[wallet_index][1]
+    nonce = w3.eth.getTransactionCount(address)
+    update_transaction = contract_instance_ERC1155.functions.giveNFTS(address, 1, 1).buildTransaction(
+        {
+        'gas': 1800000,
+        'gasPrice': w3.toWei('50', 'gwei'),
+        'from': address,
+        'nonce': nonce
+        }
+    )
+    sign_transaction = w3.eth.account.sign_transaction(update_transaction, private_key = privatekey)
+    transaction_hash = w3.eth.send_raw_transaction(sign_transaction.rawTransaction)
+    return {"address":address, "private_key":privatekey, "hash" :w3.toHex(transaction_hash)}
+
+
+
+@app.get("/riceCooker/{customer_id}")
+async def getcoin(customer_id: str, price: int):
+    users = contract_instance.functions.show_user().call()
+    wallet_index = 0
+    found = 0
+    for i in users:
+        if i == customer_id:
+            wallet_index = found
+        else:
+            found += 1
+    address = hardwallet[wallet_index][0]
+    privatekey = hardwallet[wallet_index][1]
+    nonce = w3.eth.getTransactionCount(address)
+    update_transaction = contract_instance_ERC1155.functions.giveNFTS(address, 2, 1).buildTransaction(
+        {
+        'gas': 1800000,
+        'gasPrice': w3.toWei('50', 'gwei'),
+        'from': address,
+        'nonce': nonce
+        }
+    )
+    sign_transaction = w3.eth.account.sign_transaction(update_transaction, private_key = privatekey)
+    transaction_hash = w3.eth.send_raw_transaction(sign_transaction.rawTransaction)
+    return {"address":address, "private_key":privatekey, "hash" :w3.toHex(transaction_hash)}
+
+
+
+@app.get("/iPhone13/{customer_id}")
+async def getcoin(customer_id: str, price: int):
+    users = contract_instance.functions.show_user().call()
+    wallet_index = 0
+    found = 0
+    for i in users:
+        if i == customer_id:
+            wallet_index = found
+        else:
+            found += 1
+    address = hardwallet[wallet_index][0]
+    privatekey = hardwallet[wallet_index][1]
+    nonce = w3.eth.getTransactionCount(address)
+    update_transaction = contract_instance_ERC1155.functions.giveNFTS(address, 3, 1).buildTransaction(
+        {
+        'gas': 1800000,
+        'gasPrice': w3.toWei('50', 'gwei'),
+        'from': address,
+        'nonce': nonce
+        }
+    )
+    sign_transaction = w3.eth.account.sign_transaction(update_transaction, private_key = privatekey)
+    transaction_hash = w3.eth.send_raw_transaction(sign_transaction.rawTransaction)
+    return {"address":address, "private_key":privatekey, "hash" :w3.toHex(transaction_hash)}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
